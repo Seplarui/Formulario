f74f360a52af0fe516b93147b3febec8d91f61ec
Sebas Sebas <seplarui@gmail.com>
2017-07-13 23:27:41 +0200

    Prueba PHP

---
+++ form.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sepla
+ * Date: 13/07/2017
+ * Time: 23:18
+ */
+
+$asunto=$_POST['asunto'];
+$descripcion=$_POST['descripcion'];
+
+echo "Asunto ". $asunto;
+echo "<br/>";
+echo "Descripcion ". $descripcion;
---
+++ info.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sepla
+ * Date: 13/07/2017
+ * Time: 23:23
+ */
+
+phpinfo();
